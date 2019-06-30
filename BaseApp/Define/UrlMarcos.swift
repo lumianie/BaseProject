8eb941e0cf3162b6f6027b8a93719cbef9cb314f
lumianie <991836107@qq.com>
2019-06-30 10:48:14 +0800

    增加环境变量

--- BaseApp/Define/UrlMarcos.swift
+++ BaseApp/Define/UrlMarcos.swift
@@ -8,4 +8,14 @@
 
 import Foundation
 
-public let BaseUrl: String = ""
+public var BaseUrl: String {
+    #if DEBUG
+    return "http://devapi"                      //开发环境
+    #elseif TEST
+    return "http://testapi"                     //测试环境
+    #elseif PRERELEASE
+    return "http://preapi"                      //预发环境
+    #elseif RELEASE
+    return "http://prodev"                      //生产环境
+    #endif
+}
